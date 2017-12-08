9632ea1e1ac8e1d346d5adb67af93f8844e66528
sjtg <chipilej@gmail.com>
2017-12-08 19:56:06 +0200

    1)Added home views 2)Added home url

--- career_village/cvapp/views.py
+++ career_village/cvapp/views.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from .models import Post
 # Create your views here.
+
+
+def home(request):
+	homes = cvapp.objects.all()
+	return render(request, 'home.html', {'homes': homes})
