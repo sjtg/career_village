369bdb20d43edcb5c5e300b1f367075d5eaa011d
Joshua Chipile <chipilej@gmail.com>
2018-05-16 12:41:59 +0200

    Created home page view

--- career_village/cvapp/views.py
+++ career_village/cvapp/views.py
@@ -1,14 +1,37 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
-
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+#from django.contrib.auth.models import User
+from django.views import View
+from django.http import JsonResponse
+from .models import Photo
+from .forms import PhotoForm
+from django.utils import timezone
+from .filters import PhotoFilter
 
 #from .models import Post
 # Create your views here.
 
-
+#Home page Career Village
 def home(request):
-	return HttpResponse('career village')
+	photos_lists = Photo.objects.all()
+    return render(request, 'web/photo_list.html', {'photos': photos_lists})
+
+
+
+
+#upload and crop function
+class dashboardView(View):
+    def get(self, request):
+	        photos_lists = Photo.objects.all()
+	        return render(self.request, 'web/dashboard.html', {'photos': photos_lists})
+
+    def post(self, request):
+        #time.sleep(1)
+		form = PhotoForm(self.request.POST, self.request.FILES)
+		if form.is_valid():
+			    #photo = request.user
+	          	    photo = form.save()
+		    	    photo.uploaded_at = timezone.now()
+	                    data = {'is_valid': True, 'name': photo.file.name, 'url': photo.file.url}
+		else:
+		        data = {'is_valid': False}
+		return JsonResponse(data)
