63bac35c26f3c1a4ba1d30488aa94b2e3de79900
Joshua Chipile <chipilej@gmail.com>
2018-05-16 11:27:05 +0200

    Created accounts app for career village

--- career_village/accounts/views.py
+++ career_village/accounts/views.py
@@ -3,12 +3,4 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-
-from django.http import HttpResponse
-
-#from .models import Post
 # Create your views here.
-
-
-def home(request):
-	return HttpResponse('career village')
